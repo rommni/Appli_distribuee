d3ac9c257e62ededf4eca7164145f9787a32dca4
Julian Romain <rombatie@hotmail.fr>
2017-12-07 15:47:49 +0100

    Initial Commit

---
+++ app_distribuee/src/main/java/com/example/demo/AppDistribueeApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDistribueeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDistribueeApplication.class, args);
+	}
+}
