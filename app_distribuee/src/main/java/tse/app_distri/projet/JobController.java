7853b724c35408ad1de1a246358b5e5bef9e255a
tseinfo <tseinfo@info346-vm>
2017-12-10 13:35:05 +0100

    Ajout de swagger au maven et de la classe swaggerconfig

--- app_distribuee/src/main/java/tse/app_distri/projet/JobController.java
+++ app_distribuee/src/main/java/tse/app_distri/projet/JobController.java
@@ -1,20 +1,35 @@
 package tse.app_distri.projet;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+//import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 import tse.app_distri.projet.Job;
 
-@Controller
+@RestController
 @RequestMapping(path="/jobs")
+@Api(value="onlinestore", description="Operations pertaining to manage jobs")
 public class JobController {
 	@Autowired
 	private JobRepository jobRepository;
+	
+	@ApiOperation(value = "View a list of available products",response = Iterable.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
 	
 	@GetMapping(path="/list")
 	public @ResponseBody Iterable<Job> getAllJobs(){
---
+++ app_distribuee/src/main/java/tse/app_distri/projet/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package tse.app_distri.projet;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import static springfox.documentation.builders.PathSelectors.regex;
+ 
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("guru.springframework.controllers"))
+                .paths(regex("/jobs.*"))
+                .build()
+                .apiInfo(metaData());
+    }
+    private ApiInfo metaData() {
+        ApiInfo apiInfo = new ApiInfo(
+                "Humanressources Info6 API",
+                "Spring Boot REST API for Human ressources",
+                "1.0",
+                "Terms of service",
+                new Contact("JULIAN Romain & LAURIER Alexis", "https://www.telecom-st-etienne.fr", "romain.julian@telecom-st-etienne.fr"),
+               "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0");
+        return apiInfo;
+    }
+}
