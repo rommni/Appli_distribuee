3abc7f22ae696531ca527ff73fcf9cadd41143d3
Julian Romain <rombatie@hotmail.fr>
2017-12-13 16:43:40 +0100

    Web service employee fonctionnel (Q8)

--- app_distribuee/src/main/java/tse/app_distri/projet/EmployeeController.java
+++ app_distribuee/src/main/java/tse/app_distri/projet/EmployeeController.java
@@ -8,6 +8,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,11 +19,11 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeRepository employeeRepository;
 
-	@GetMapping(path="/list")
-	public @ResponseBody Iterable<EmployeeDTO> getAllEmployee(){
+	@GetMapping(path="/search")
+	public @ResponseBody Iterable<EmployeeDTO> getAllEmployee(@RequestParam String firstName){
 		ModelMapper modelMapper = new ModelMapper();
 		ArrayList<EmployeeDTO> employees = new ArrayList<EmployeeDTO>();
-		for (Iterator<Employee> i = employeeRepository.findAll().iterator(); i.hasNext();) {
+		for (Iterator<Employee> i = employeeRepository.findByFirstName(firstName).iterator(); i.hasNext();) {
 		    Employee employee = i.next();
 		    EmployeeDTO employeeDto = modelMapper.map(employee, EmployeeDTO.class);
 		    employees.add(employeeDto);
--- app_distribuee/src/main/java/tse/app_distri/projet/EmployeeRepository.java
+++ app_distribuee/src/main/java/tse/app_distri/projet/EmployeeRepository.java
@@ -1,7 +1,9 @@
 package tse.app_distri.projet;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface EmployeeRepository extends CrudRepository<Employee, String>{
-
+	List<Employee> findByFirstName(String firstName);
 }
